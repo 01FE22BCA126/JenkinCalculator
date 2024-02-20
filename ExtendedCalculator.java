44360bac77b82bcfc8458d4255899c88fb6336d9
Ritul <01fe22bca126@kletech.ac.in>
2024-02-20 09:44:55 +0530

    Code with extended calculator functions

---
+++ ExtendedCalculator.java
@@ -0,0 +1,26 @@
+/**
+ * The ExtendedCalculator class contains methods for calculating the square, cube,of a given
+ * number.
+ */
+
+ public class ExtendedCalculator{
+    /**
+     * The function "squ" takes a float operand and returns its square.
+     * 
+     * @param operand1 The parameter "operand1" is a float value that represents the number to be squared.
+     * @return the square of the operand1 value.
+     */
+       public float squ(float operand1){
+          return(operand1*operand1);
+       }
+      /**
+       * The cube function takes a float operand and returns the cube of that operand.
+       * 
+       * @param operand1 The parameter "operand1" is a float value that represents the number you want to
+       * calculate the cube of.
+       * @return The cube of the operand1 value.
+       */
+       public float cube(float operand1){
+          return(operand1*operand1*operand1);
+       }
+    }
